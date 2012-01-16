d82b7810158f6489d36a2453a9d3d5123741efa1
Senpai <joel@transparatech.com>
2012-01-15 23:04:08 -0800

    Turning off the ability to add modules and themes via the Drupal UI. From now on, installation only occurs via Git. Signed-off-by:Senpai <joel@transparatech.com>
    Signed-off-by: Senpai <joel@transparatech.com>

--- sites/default/default.settings.php
+++ sites/default/default.settings.php
@@ -486,7 +486,7 @@ $conf['404_fast_html'] = '<html xmlns="http://www.w3.org/1999/xhtml"><head><titl
  *
  * Remove the leading hash signs to disable.
  */
-# $conf['allow_authorize_operations'] = FALSE;
+$conf['allow_authorize_operations'] = FALSE;
 
 /**
  * Smart start:
