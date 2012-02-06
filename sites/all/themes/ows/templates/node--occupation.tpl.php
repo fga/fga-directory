8bcdca2c760938ec2b45efdd4016dec7ba503399
Trevor Twining <trevortwining@gmail.com>
2012-02-05 22:18:28 -0500

    #151: Finishing 1st pass at node/site theme

---
+++ sites/all/themes/ows/templates/node--occupation.tpl.php
@@ -0,0 +1,52 @@
+<article<?php print $attributes; ?>>
+  <?php print $user_picture; ?>
+  
+  <?php if (!$page && $title): ?>
+  <header>
+    <?php print render($title_prefix); ?>
+    <h2<?php print $title_attributes; ?>><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
+    <?php print render($title_suffix); ?>
+  </header>
+  <?php endif; ?>
+  
+  <?php if ($display_submitted): ?>
+  <footer class="submitted"><?php print $date; ?> -- <?php print $name; ?></footer>
+  <?php endif; ?>  
+  
+  <div class="content-grid container-12">
+    <div<?php print $content_attributes; ?>>
+      <?php
+        // We hide the comments and links now so that we can render them later.
+        hide($content['comments']);
+        hide($content['links']);
+        hide($content['group_links_social']);
+        hide($content['group_links_news']);
+        hide($content['group_links_resources']);
+        hide($content['group_links_media']);
+        print render($content);
+      ?>
+      
+      <div class="clearfix">
+    <?php if (!empty($content['links'])): ?>
+      <nav class="links node-links clearfix"><?php print render($content['links']); ?></nav>
+    <?php endif; ?>
+
+    <?php print render($content['comments']); ?>
+  </div>
+      
+    </div>
+    
+    <div class="occupy-links-primary grid-3">
+      <?php
+        print render($content['group_links_social']);
+        print render($content['group_links_media']);
+        ?>
+    </div>
+    <div class="occupy-links-secondary grid-3">
+      <?php
+        print render($content['group_links_news']);
+        print render($content['group_links_news']);
+        ?>
+    </div>
+  </div>
+</article>
