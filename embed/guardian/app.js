009f932d78448cecfbfc2ac1d5e45b6d44890bb4
Thiago de Mello Bueno <thiago@madeofpeople.org>
2012-05-04 14:46:50 -0400

    Toying with mappbox for guardian embed.

--- embed/guardian/app.js
+++ embed/guardian/app.js
@@ -1,49 +1,51 @@
-var url = 'http://api.tiles.mapbox.com/v3/occupy.occupy-streets.jsonp';
-var m;
+jQuery(document).ready(function($) {
 
-wax.tilejson(url, function(tilejson) {
-  m = new MM.Map('map', new wax.mm.connector(tilejson));
+	var url = 'http://api.tiles.mapbox.com/v3/occupy.occupy-streets.jsonp';
+	var m;
 
-  wax.mm.interaction()
-    .map(m)
-    .tilejson(tilejson)
-    .on(wax.tooltip().parent(m.parent).events());
+	wax.tilejson(url, function(tilejson) {
+	  m = new MM.Map('map', new wax.mm.connector(tilejson));
+		console.log( wax.mm.interaction() );
+	  wax.mm.interaction()
+	    .map(m)
+	    .tilejson(tilejson)
+	    .on( wax.movetip().parent( m.parent ).events() );
+	  m.setZoomRange( 1.9, 6);
+	  m.setCenterZoom({ lat: 19, lon: 9.7 }, 3 );
+	});
 
-  m.setZoomRange(1.9, 10);
-  m.setCenterZoom({ lat: 19, lon: 9.7 }, 1.9);
-});
+	$(function() {
 
+		// Zoom into location
+		function geocode(query) {
+			$.ajax({
+				url: 'http://open.mapquestapi.com/nominatim/v1/search?format=json&json_callback=callback&limit=1&q=' + query,
+				dataType: 'jsonp',
+				jsonpCallback: 'callback',
+				success: function (value) {
+					value = value[0];
+					if (value === undefined) {
+						alert('The search you tried did not return a result.');
+					} else {
+						console.log( value );
+						easey().map(m)
+						.to(m.locationCoordinate({ lat: value.lat, lon: value.lon })
+						.zoomTo(6))
+						.run(4000);
+					}
+				}
+			});
+		}
 
-$(function() {
+		var input = $('input.search');
+		$('form.location').submit(function (e){
+			e.preventDefault();
+			var inputValue = input.val(),
+			encodedInput = encodeURIComponent(inputValue);
+			geocode(encodedInput);
+		});
 
-  // Zoom into location
-  function geocode(query) {
-    $.ajax({
-      url: 'http://open.mapquestapi.com/nominatim/v1/search?format=json&json_callback=callback&limit=1&q=' + query,
-      dataType: 'jsonp',
-      jsonpCallback: 'callback',
-      success: function (value) {
-        value = value[0];
-        if (value === undefined) {
-          alert('The search you tried did not return a result.');
-        } else {
-            easey().map(m)
-              .to(m.locationCoordinate({ lat: value.lat, lon: value.lon })
-              .zoomTo(6))
-              .run(4000);
-        }
-      }
-    });
-  }
+	});
 
-  var input = $('input.search');
+})
 
-console.log(input);
-
-  $('form.location').submit(function (e){
-    e.preventDefault();
-    var inputValue = input.val(),
-    encodedInput = encodeURIComponent(inputValue);
-    geocode(encodedInput);
-  });
-});
