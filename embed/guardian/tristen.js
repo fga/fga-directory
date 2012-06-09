11b675517c0ef519e338170f511c6c3d5eb5e0f1
Andrew_Mallis <andrew@ideograph.biz>
2012-06-08 20:37:16 -0700

    more refactoring for clarity and moving some assets around

---
+++ embed/guardian/tristen.js
@@ -0,0 +1,61 @@
+
+
+// var url = 'http://api.tiles.mapbox.com/v3/occupydirectory.map-lzygvfea.jsonp';
+var url = 'http://api.tiles.mapbox.com/v3/occupy.occupy-streets.jsonp';
+
+var m;
+
+wax.tilejson(url, function(tilejson) {
+  m = new MM.Map('map', new wax.mm.connector(tilejson));
+
+  wax.mm.interaction()
+    .map(m)
+    .tilejson(tilejson)
+    .on(wax.tooltip().parent(m.parent).events());
+
+	  m.setZoomRange( 1.9, 11);
+	  m.setCenterZoom({ lat: 19, lon: 9.7 }, 2 );
+	  
+	  
+ 	  // Add zoom controls [?]
+    wax.mm.zoomer(map, tilejson).appendTo(map.parent);
+
+     // Add interactivity (tooltips)
+    var interaction = wax.mm.interaction().map(map).tilejson(tilejson)
+        .on(wax.tooltip().parent(map.parent).events());
+});
+
+
+$(function() {
+
+  // Zoom into location
+  function geocode(query) {
+    $.ajax({
+      url: 'http://open.mapquestapi.com/nominatim/v1/search?format=json&json_callback=callback&limit=1&q=' + query,
+      dataType: 'jsonp',
+      jsonpCallback: 'callback',
+      success: function (value) {
+        value = value[0];
+        if (value === undefined) {
+          alert('<p>The search you tried did not return a result.</p>');
+        } else {
+            easey().map(m)
+              .to(m.locationCoordinate({ lat: value.lat, lon: value.lon })
+              .zoomTo(8))
+              .run(4000);
+        }
+      }
+    });
+  }
+
+  var input = $('input.search');
+
+console.log(input);
+
+  $('form.location').submit(function (e){
+    e.preventDefault();
+    var inputValue = input.val(),
+    encodedInput = encodeURIComponent(inputValue);
+    geocode(encodedInput);
+  });
+});
