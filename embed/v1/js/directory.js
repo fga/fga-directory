d3e3e2a82c8685da291abfaa81ab361a56917c35
Thiago de Mello Bueno <thiago@madeofpeople.org>
2012-09-23 20:21:42 -0400

    Added share window using updated embed code with piwiki tracking

--- embed/v1/js/directory.js
+++ embed/v1/js/directory.js
@@ -6,11 +6,14 @@ var directory = directory || {};
 
 directory.tooltipTemplate = "<div class=\"detail\" id=\"occupation-{{{id}}}\">    <!-- using hCard microformat -->  <div class=\"vcard\">        <div class=\"occupation-title clearfix\">	    {{#Occupation_image-thumb}}      <div class=\"occupation-image\">        <img src=\"{{{Occupation_image-thumb}}}\" width=\"92\" height=\"92\" alt=\"{{{Title}}} image\" />      </div>	    {{/Occupation_image-thumb}}      <h3 class=\"fn org title\">      <a class=\"occupation-directory-url\"  href=\"{{{Occupy Directory URL}}}\">{{{Title}}}</a>      </h3>    </div>        <ul>      {{#Occupation website}}      <li class=\"website-url taglink\">        <a class=\"url\" title=\"Occupation Website\" target=\"_blank\" href=\"{{{Occupation website}}}\">Occupation website</a>      </li>      {{/Occupation website}}      {{#General email address}}      <li class=\"general-email-address taglink\">        <a class=\"email\" href=\"mailto:{{{General email address}}}\">{{{General email address}}}</a>      </li>      {{/General email address}}      <li class=\"adr\">                <div class=\"street\">          {{#Location Name}}<span class=\"locality\">{{{Location Name}}}</span>{{/Location Name}}          {{#Address 1}}<span class=\"street-address\">{{{Address 1}}}</span>{{/Address 1}}          {{#Address 2}}<span class=\"extended-address\">{{{Address 2}}}</span>{{/Address 2}}        </div>                <span class=\"loc\">          {{#City}}<span class=\"city\">{{{City}}}</span>{{/City}}{{#City_sub}}<span class=\"citi-sub\"> {{{City_sub}}}</span>{{/City_sub}}{{#State}}<span class=\"region\">, {{{State}}}</span> {{/State}}{{#State_sub}}<span class=\"state-sub\"> {{{State_sub}}}</span>{{/State_sub}}{{#zip-postal}}<span class=\"postal-code\">{{{zip-postal}}}</span> {{/zip-postal}}<br/>          {{#Country}}<span class=\"country-name\">{{{Country}}}</span>{{/Country}}        </span>      </li>      {{#General phone}}      <li class=\"tel general-phone taglink\">        {{{General phone}}}      </li>      {{/General phone}}    </ul>  </div><!--end vcard -->  <div class=\"info\">    {{#Founded-longdate}}    <div class=\"founded YYYY-MM-DD\">      <h5 class=\"label\">Founded</h5>      <span class=\"bday\">{{{Founded-longdate}}}</span>    </div>    {{/Founded-longdate}}            {{#Occupation description}}<p class=\"occupation-description\">{{{Occupation description}}}</p>{{/Occupation description}}    {{#General Assembly}}<p class=\"genera-assembly\">{{{General Assembly}}}</p>{{/General Assembly}}    {{#Physical occupation occurring}}<p class=\"physical-occupation-occurring\">{{{Physical occupation occurring}}}</p>{{/Physical occupation occurring}}  </div><!-- end info -->  <ul class=\"listings clearfix\">    <li>      {{#occupation_websites_extra1}}      <h5 class=\"label\">Additional Websites</h5>      <ul class=\"additional-websites clearfix\">        <li class=\"taglink\"><a target=\"_blank\" href=\"{{{occupation_websites_extra1}}}\">social 1</a></li>        {{#occupation_websites_extra2}}<li class=\"taglink\"><a target=\"_blank\" href=\"{{{occupation_websites_extra2}}}\">social 2</a></li>{{/occupation_websites_extra2}}        {{#occupation_websites_extra3}}<li class=\"taglink\"><a target=\"_blank\" href=\"{{{occupation_websites_extra3}}}\">social 3</a></li>{{/occupation_websites_extra3}}      </ul>      {{/occupation_websites_extra1}}      {{#Twitter_hashtag1}}      <h5 class=\"label\">Twitter Hash Tags</h5>      <ul class=\"twitter-hashtags clearfix\">        <li class=\"twitter-hashtags taglink\"><a target=\"_blank\" href=\"#\">{{{Twitter_hashtag1}}}</a></li>        {{#Twitter_hashtag2}}<li class=\"twitter-hashtags taglink\"><a target=\"_blank\" href=\"#\">{{{Twitter_hashtag2}}}</a></li>{{/Twitter_hashtag2}}        {{#Twitter_hashtag3}}<li class=\"twitter-hashtags taglink\"><a target=\"_blank\" href=\"#\">{{{Twitter_hashtag3}}}</a></li>{{/Twitter_hashtag3}}      </ul>      {{/Twitter_hashtag1}}            {{#Twitter_account1}}      <h5 class=\"label\">Twitter Accounts</h5>      <ul class=\"twitter-accounts clearfix\">       <li class=\"twitter-accounts taglink\"><a target=\"_blank\" href=\"http://twitter.com/{{{Twitter_account1}}}\">{{{Twitter_account1}}}</a></li>        {{#Twitter_account2}}<li class=\"twitter-accounts taglink\"><a target=\"_blank\" href=\"http://twitter.com/{{{Twitter_account2}}}\">{{{Twitter_account2}}}</a></li>{{/Twitter_account2}}        {{#Twitter_account3}}<li class=\"twitter-accounts taglink\"><a target=\"_blank\" href=\"http://twitter.com/{{{Twitter_account3}}}\">{{{Twitter_account3}}}</a></li>{{/Twitter_account3}}      </ul>      {{/Twitter_account1}}                  {{#Youtube1}}      <h5 class=\"label\">Youtube Channels</h5>      <ul class=\"youtube-sites clearfix\">        <li class=\"youtube-channel-urls taglink\"><a target=\"_blank\" href=\"{{{Youtube1}}}\">Channel 1</a></li>        {{#Youtube2}}<li class=\"youtube-channel-urls taglink\"><a target=\"_blank\" href=\"{{{Youtube2}}}\">Channel 2</a></li>{{/Youtube2}}      </ul>      {{/Youtube1}}            {{#live_video1}}      <h5 class=\"label\">Video Streams</h5>      <ul class=\"video-sites clearfix\">        <li class=\"live-video-streams taglink\"><a target=\"_blank\" href=\"{{{live_video1}}}\">Stream 1</a></li>        {{#live_video2}}<li class=\"live-video-streams taglink\"><a target=\"_blank\" href=\"{{{live_video2}}}\">Stream 2</a></li>{{/live_video2}}      </ul>      {{/live_video1}}      <h5 class=\"label\">Social Media</h5>      <ul class=\"social-media-sites clearfix\">        {{#Facebook1}}<li class=\"facebook-pages taglink\"><a  target=\"_blank\" href=\"{{{Facebook1}}}\">Primary Facebook</a></li>{{/Facebook1}}        {{#Facebook2}}<li class=\"facebook-pages taglink\"><a  target=\"_blank\" href=\"{{{Facebook2}}}\">Other Facebook</a></li>{{/Facebook2}}        {{#Meetup URL}}<li class=\"meetup-url taglink\"><a  target=\"_blank\" href=\"{{{Meetup URL}}}\">MeetUp</a></li>{{/Meetup URL}}        {{#social_media_extra1}}<li class=\"additional-social-media-sites taglink\"><a  target=\"_blank\" href=\"{{{social_media_extra1}}}\">Other Social Media A</a></li>{{/social_media_extra1}}        {{#social_media_extra2}}<li class=\"additional-social-media-sites taglink\"><a  target=\"_blank\" href=\"{{{social_media_extra2}}}\">Other Social Media B</a></li>{{/social_media_extra2}}        {{#social_media_extra3}}<li class=\"additional-social-media-sites taglink\"><a  target=\"_blank\" href=\"{{{social_media_extra3}}}\">Other Social Media C</a></li>{{/social_media_extra3}}              </ul>      {{#Newspaper URL}}      <h5 class=\"label\">Newspapers</h5>      <ul class=\"newspapers clearfix\">        <li class=\"newspaper-urls taglink\"><a target=\"_blank\"  href=\"{{{Newspaper URL}}}\">Newspaper</a></li>      </ul>      {{/Newspaper URL}}      {{#RSS1}}      <h5 class=\"label\">RSS Feeds</h5>      <ul class=\"feeds clearfix\">        <li class=\"rss-feeds taglink\"><a target=\"_blank\" href=\"{{{RSS1}}}\">RSS 1</a></li>        {{#RSS2}}<li class=\"rss-feeds taglink\"><a target=\"_blank\" href=\"{{{RSS2}}}\">RSS 2</a></li>{{/RSS2}}</ul>{{/RSS1}}        </li>  </ul><!-- end listing --></div><!-- end tooltip-inner -->";
 
+
 if (!window.console ){
 	var names = ["log", "debug", "info", "warn", "error", "assert", "dir", "dirxml", "group", "groupEnd", "time", "timeEnd", "count", "trace", "profile", "profileEnd"];
  	window.console = {};
-  for (var i = 0; i < names.length; ++i){ window.console[names[i]] = function() {}; }
-	names = null;
+    for (var i = 0; i < names.length; ++i){ 
+        window.console[names[i]] = function() {};
+    }
+    names = null;
 }
 
 	directory.movetip = function() {
@@ -124,35 +127,112 @@ if (!window.console ){
     return t;
 };
 
-		// override mabox interaction auto... not the best way, but seems to work fine.
-		// kind of tangly to cut through the layers otherwise
-		mapbox.interaction = function() {
+// override mabox interaction auto... not the best way, but seems to work fine.
+// kind of tangly to cut through the layers otherwise
+mapbox.interaction = function() {
 
-		    var interaction = wax.mm.interaction(),
-		        auto = false;
+    var interaction = wax.mm.interaction(),
+        auto = false;
 
-		    interaction.refresh = function() {
-		        var map = interaction.map();
-		        if (!auto || !map) return interaction;
-		        for (var i = map.layers.length - 1; i >= 0; i --) {
-		            var tj = map.layers[i].tilejson && map.layers[i].tilejson();
-		            if (tj && tj.template) return interaction.tilejson(tj);
-		        }
-		        return interaction.tilejson({});
-		    };
+    interaction.refresh = function() {
+        var map = interaction.map();
+        if (!auto || !map) return interaction;
+        for (var i = map.layers.length - 1; i >= 0; i --) {
+            var tj = map.layers[i].tilejson && map.layers[i].tilejson();
+            if (tj && tj.template) return interaction.tilejson(tj);
+        }
+        return interaction.tilejson({});
+    };
 
-		    interaction.auto = function() {
-		        auto = true;
-		        interaction.on( directory.movetip()
-		            .parent(interaction.map().parent)
-		            .events());
-		        return interaction.refresh();
-		    };
+    interaction.auto = function() {
+        auto = true;
+        interaction.on( directory.movetip()
+            .parent(interaction.map().parent)
+            .events());
+        return interaction.refresh();
+    };
 
-		    return interaction;
-		};
-		
-		directory.initMap = function( el ){
+    return interaction;
+};
+
+
+directory.share = function(map, tilejson) {
+    tilejson = tilejson || {};
+    var l = window.location;
+    tilejson.webpage = l.href;
+    tilejson.embed = (l.hash) ? l.href + '?embed' : l.href + '#/?embed';
+    var link = document.createElement('a');
+    var close = document.createElement('a');
+    var embed = document.createElement('textarea');
+    var share = document.createElement('div');
+    var popup = document.createElement('div');
+    var elements = [link, close, embed, share, popup];
+
+    if (typeof com !== 'undefined') {
+        var mm = com.modestmaps;
+        for (var i = 0; i < elements.length; i++) {
+            mm.addEvent(elements[i], 'mousedown', function(e) { mm.cancelEvent(e); });
+            mm.addEvent(elements[i], 'dblclick', function(e) { mm.cancelEvent(e); });
+        }
+    }
+
+    link.innerHTML = 'Share +';
+    link.href = '#';
+    link.className = 'share';
+    link.onclick = link.ontouchstart = function() {
+        popup.style.display = popup.style.display === 'block' ? 'none' : 'block';
+        $('body').toggleClass('sharing');
+        return false;
+    };
+
+    close.innerHTML = 'Close';
+    close.href = '#';
+    close.className = 'close';
+    close.onclick = close.ontouchstart = function() {
+        popup.style.display = popup.style.display === 'block' ? 'none' : 'block';
+        $('body').toggleClass('sharing');
+        return false;
+    };
+
+    embed.className = 'embed';
+    embed.rows = 4;
+    embed.setAttribute('readonly', 'readonly');
+    embed.value = '<div id="embed-wrapper" style="width:960px; height:700px"><iframe id="directory-embed" width="960" height="600" src="{{embed}}" style="border:0; height:100%; width:100%; position:absolute;" marginwidth="0" marginheight="0" frameborder="0" vspace="0" hspace="0" allowtransparency="true" scrolling="no" ></iframe><script type="text/javascript">document.getElementById("directory-embed").src = "{{embed}}" + "phostname=" + window.location.hostname;</script></div>'.replace('{{embed}}', tilejson.embed + "&phostname=" + window.location.hostname );
+    embed.onclick = function() {
+        embed.focus();
+        embed.select();
+        return false;
+    };
+
+    var twitter = 'http://twitter.com/intent/tweet?status='
+        + encodeURIComponent(document.title + ' (' + tilejson.webpage + ')');
+    var facebook = 'https://www.facebook.com/sharer.php?u='
+        + encodeURIComponent(tilejson.webpage)
+        + '&t=' + encodeURIComponent(document.title);
+    share.innerHTML = ('<h3>Share this map</h3>'
+        + '<p><a class="facebook" target="_blank" href="{{facebook}}">Facebook</a>'
+        + '<a class="twitter" target="_blank" href="{{twitter}}">Twitter</a></p>')
+        .replace('{{twitter}}', twitter)
+        .replace('{{facebook}}', facebook);
+    share.innerHTML += '<strong>Get the embed code</strong><br />'
+    share.innerHTML += '<small>Copy and paste this HTML into your website or blog.</small>';
+    share.appendChild(embed);
+    share.appendChild(close);
+
+    popup.className = 'wax-share';
+    popup.style.display = 'none';
+    popup.appendChild(share);
+
+    return {
+        appendTo: function(elem) {
+            wax.u.$(elem).appendChild(link);
+            wax.u.$(elem).appendChild(popup);
+            return this;
+        }
+    };
+};
+
+directory.initMap = function( el ){
 
 			directory.map.container = el;
 
@@ -162,10 +242,10 @@ if (!window.console ){
 
 			mapbox.load( directory.map.id, function( o ){
 				var eventHandlers = [
-					easey_handlers.TouchHandler(),
-        	easey_handlers.DragHandler(),
-        	easey_handlers.DoubleClickHandler()
-        ]
+                    easey_handlers.TouchHandler(),
+                    easey_handlers.DragHandler(),
+                    easey_handlers.DoubleClickHandler()
+                ]
 				var map = mapbox.map('map', null, null, eventHandlers );
                 directory.map.map = map;
 				map.addLayer( o.layer ).zoom( directory.map.zoom.init ).center( directory.map.center );				
@@ -182,15 +262,18 @@ if (!window.console ){
 						}
 					}
 				});
+                if( $.inArray('share', directory.map.options ) > -1 ){
+                    directory.share( map,  map.layers[0].tilejson() ).appendTo( $( 'body' )[0] );
+                    directory.map.options.splice($.inArray( "share", directory.map.options),1);
+                }
 				$( directory.map.options ).each( function( i, opt ){
 					map.ui[this].add();
 				});
 				map.setPanLimits( directory.map.bounds );
 				map.setZoomRange( directory.map.zoom.min, directory.map.zoom.max );				
-        directory.geocoder( map );
+                directory.geocoder( map );
 				map.refresh();
 			});
-
 			
 		}
 		
@@ -251,8 +334,7 @@ directory.geocoder = function() {
                         }
                     }
                                         
-                    if (callback && typeof(callback) == 'string') 
-                        executeFunctionByName(callback, window);
+                    if (callback && typeof(callback) == 'string') executeFunctionByName(callback, window);
                 }
             }
 
@@ -260,12 +342,11 @@ directory.geocoder = function() {
     };
 };
 
-
 $( function(){
-	directory.initMap( $('#map')[0] );
     if( $( '#embed_aux' ) ){
         var el = $('#embed_aux');
         directory.footerHeight = el.height() + parseInt( el.css('padding-top') ) * 2;
     }
+	directory.initMap( $('#map')[0] );
 });
 
