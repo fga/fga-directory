c883ccddecc3e0a3b89955e3e0eea5c26cedab5b
Thiago de Mello Bueno <thiago@madeofpeople.org>
2012-11-13 21:39:49 -0800

    resolved some jankyness with directory_map.tpl.php

--- sites/all/modules/custom/directory/directory_map.tpl.php
+++ sites/all/modules/custom/directory/directory_map.tpl.php
@@ -8,5 +8,3 @@ Make src configurable from Drups $iframe_src
 <div id="map-embed-wrapper">
   <iframe id="map-embed" src="http://directory.occupy.net/embed/directory/index.html" marginwidth="0" marginheight="0" scrolling="no"></iframe>
 </div>
-
-<?php
