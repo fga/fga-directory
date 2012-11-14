0c4fed449b363b52f9fc41acec629598fb33a170
Andrew_Mallis <andrew@ideograph.ca>
2012-11-13 20:15:31 -0800

    adding missing php to tpl in directory module

--- sites/all/modules/custom/directory/directory_map.tpl.php
+++ sites/all/modules/custom/directory/directory_map.tpl.php
@@ -1,3 +1,4 @@
+<?php
 <div id="map-embed-wrapper">
   <iframe id="map-embed" src="http://directory.occupy.net/embed/directory/index.html" marginwidth="0" marginheight="0" scrolling="no"></iframe>
 </div>
