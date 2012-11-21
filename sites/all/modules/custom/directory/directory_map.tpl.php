ece03008ff025a20b341e99b495bd5df78c81191
Andrew_Mallis <andrew@ideograph.ca>
2012-11-21 13:24:31 -0800

    better iframe properties for map

--- sites/all/modules/custom/directory/directory_map.tpl.php
+++ sites/all/modules/custom/directory/directory_map.tpl.php
@@ -5,6 +5,6 @@ Make src configurable from Drups $iframe_src
 */
 ?>
 
-<div id="directory-map-wrapper">
-  <iframe id="map-embed" src="http://directory.occupy.net/embed/directory/index.html" marginwidth="0" marginheight="0" scrolling="no"></iframe>
+<div id="directory-map-wrapper" style="width:100%;">
+  <iframe id="map-embed" src="http://directory.occupy.net/embed/directory/index.html" style="border:0; height:100%; width:100%; position:absolute;" marginwidth="0" marginheight="0" frameborder="0" vspace="0" hspace="0" allowtransparency="true" scrolling="no" ></iframe>
 </div>
