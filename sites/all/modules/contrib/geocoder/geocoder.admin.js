170c71e73253c8933ac7e8f07ead29c2c486731d
Andrew_Mallis <andrew@ideograph.ca>
2012-11-30 16:46:59 -0800

    update geocoder and geophp. geophp libarary is not part of geophp, but views_geojson complains that it can't find it there, so we keep our version in /libraries also.

--- sites/all/modules/contrib/geocoder/geocoder.admin.js
+++ sites/all/modules/contrib/geocoder/geocoder.admin.js
@@ -1,44 +1,42 @@
-jQuery(document).ready(function(){
-	geocoder_admin_field_selected();
-	geocoder_admin_handler_selected();
-});
+(function ($) {
 
-function geocoder_admin_field_selected() {
-  var field = jQuery('#edit-instance-widget-settings-geocoder-field').val();
-  var field_type = Drupal.settings.geocoder_widget_settings.types[field];
-  var valid_handlers = Drupal.settings.geocoder_widget_settings.handlers[field_type];
-  
-  // Filter the options list to ones that are valid for this field
-  jQuery('#edit-instance-widget-settings-geocoder-handler option').each(function() {
-  	handler_type = jQuery(this).val();
-  	if (geocoder_admin_handler_in_array(handler_type,valid_handlers)) {
-  	  jQuery(this).attr('disabled',false);
-  	  jQuery(this).show();
-  	}
-  	else {
-  		jQuery(this).attr('disabled','disabled');
-  		jQuery(this).hide();
-  	}
-  });
-  
-  // If the currently selected handler is not valid, set it to the first valid handler
-  if (!geocoder_admin_handler_in_array(jQuery('#edit-instance-widget-settings-geocoder-handler').val(),valid_handlers)) {
-  	jQuery('#edit-instance-widget-settings-geocoder-handler').val(valid_handlers[0]);
-  }
-  
-  geocoder_admin_handler_selected();
-}
+  Drupal.behaviors.geocoderWidgetSettings = {
+    attach: function (context, settings) {
+      $(':input[name="instance[widget][settings][geocoder_field]"]', context).once('geofield_widget_settings', function () {
+        // When the geocoder_field field is changed, update the geocoder_handler options.
+        $(this).change(function() {
+          var field = $(this).val();
+          var field_type = settings.geocoder_widget_settings.types[field];
+          var valid_handlers = settings.geocoder_widget_settings.handlers[field_type];
+          $(':input[name="instance[widget][settings][geocoder_handler]"] option', context).each(function () {
+            var handler_type = $(this).val();
+            if (geocoder_admin_handler_in_array(handler_type, valid_handlers)) {
+              $(this).removeAttr('disabled').show();
+            }
+            else {
+              $(this).attr('disabled', 'disabled').hide();
+            }
+          });
 
-function geocoder_admin_handler_in_array(needle, haystack) {
-  var length = haystack.length;
-  for(var i = 0; i < length; i++) {
-    if(haystack[i] == needle) return true;
-  }
-  return false;
-}
+          // If the currently selected handler is not valid, set it to the first valid handler.
+          // Trigger a change event so that depdendent fields will update.
+          if (!geocoder_admin_handler_in_array($(':input[name="instance[widget][settings][geocoder_handler]"]').val(), valid_handlers)) {
+            $('#:input[name="instance[widget][settings][geocoder_handler]"]').val(valid_handlers[0]).trigger('change');
+          }
+        });
 
-function geocoder_admin_handler_selected() {
-  var handler = jQuery('#edit-instance-widget-settings-geocoder-handler').val();
-  jQuery('.geocoder-handler-setting').hide();
-  jQuery('.geocoder-handler-setting-' + handler).show();
-}
+        function geocoder_admin_handler_in_array(needle, haystack) {
+          var length = haystack.length;
+          for(var i = 0; i < length; i++) {
+            if(haystack[i] == needle) return true;
+          }
+          return false;
+        }
+      });
+      // Trigger an initial change event to initialize the dependent field.
+      $(this).trigger('change');
+    }
+  };
+
+})(jQuery);
+
