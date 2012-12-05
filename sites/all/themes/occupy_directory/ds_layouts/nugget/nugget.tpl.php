0e3b11c4b2cfc5c27b119544cf04e177072b6332
Andrew_Mallis <andrew@ideograph.ca>
2012-12-05 05:18:33 -0800

    Display suite templates for occupation and a nugget tbd.

---
+++ sites/all/themes/occupy_directory/ds_layouts/nugget/nugget.tpl.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * @file
+ * Display Suite nugget template.
+ *
+ * Available variables:
+ *
+ * Layout:
+ * - $classes: String of classes that can be used to style this layout.
+ * - $contextual_links: Renderable array of contextual links.
+ * - $layout_wrapper: wrapper surrounding the layout.
+ *
+ * Regions:
+ *
+ * - $left: Rendered content for the "left" region.
+ * - $left_classes: String of classes that can be used to style the "left" region.
+ *
+ * - $middle: Rendered content for the "middle" region.
+ * - $middle_classes: String of classes that can be used to style the "middle" region.
+ *
+ * - $right: Rendered content for the "right" region.
+ * - $right_classes: String of classes that can be used to style the "right" region.
+ */
+?>
+<<?php print $layout_wrapper; print $layout_attributes; ?> class="<?php print $classes;?> clearfix">
+
+  <!-- Needed to activate contextual links -->
+  <?php if (isset($title_suffix['contextual_links'])): ?>
+    <?php print render($title_suffix['contextual_links']); ?>
+  <?php endif; ?>
+
+    <<?php print $left_wrapper; ?> class="ds-left<?php print $left_classes; ?>">
+      <?php print $left; ?>
+    </<?php print $left_wrapper; ?>>
+
+    <<?php print $middle_wrapper; ?> class="ds-middle<?php print $middle_classes; ?>">
+      <?php print $middle; ?>
+    </<?php print $middle_wrapper; ?>>
+
+    <<?php print $right_wrapper; ?> class="ds-right<?php print $right_classes; ?>">
+      <?php print $right; ?>
+    </<?php print $right_wrapper; ?>>
+
+</<?php print $layout_wrapper ?>>
+
+<!-- Needed to activate display suite support on forms -->
+<?php if (!empty($drupal_render_children)): ?>
+  <?php print $drupal_render_children ?>
+<?php endif; ?>
