bde943832fd2cfb2afc162ac9b7e07967802c7b8
Andrew_Mallis <andrew@ideograph.ca>
2012-12-19 15:02:47 -0800

    update search results with new DS field order, add DS template, update CSS a bit

---
+++ sites/all/themes/occupy_directory/ds_layouts/search_result/search-result.tpl.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @file
+ * Display Suite search-result template.
+ *
+ * Available variables:
+ *
+ * Layout:
+ * - $classes: String of classes that can be used to style this layout.
+ * - $contextual_links: Renderable array of contextual links.
+ * - $layout_wrapper: wrapper surrounding the layout.
+ *
+ * Regions:
+ *
+ * - $left: Rendered content for the "left" region.
+ * - $left_classes: String of classes that can be used to style the "left" region.
+ *
+ * - $middle: Rendered content for the "middle" region.
+ * - $middle_classes: String of classes that can be used to style the "middle" region.
+ *
+ * - $right: Rendered content for the "right" region.
+ * - $right_classes: String of classes that can be used to style the "right" region.
+ */
+
+  $classes = 'search-result clearfix';
+  $left_classes = 'first equal-height-element';
+  $right_classes  = 'equal-height-element';
+
+
+?>
+<<?php print $layout_wrapper; print $layout_attributes; ?> class="<?php print $classes;?>">
+
+  <!-- Needed to activate contextual links -->
+  <?php if (isset($title_suffix['contextual_links'])): ?>
+    <?php print render($title_suffix['contextual_links']); ?>
+  <?php endif; ?>
+
+    <<?php print $left_wrapper; ?> class="ds-left <?php print $left_classes; ?>">
+      <?php print $left; ?>
+    </<?php print $left_wrapper; ?>>
+
+
+    <<?php print $right_wrapper; ?> class="ds-right <?php print $right_classes; ?>">
+      <?php print $right; ?>
+    </<?php print $right_wrapper; ?>>
+
+</<?php print $layout_wrapper ?>>
+
+<!-- Needed to activate display suite support on forms -->
+<?php if (!empty($drupal_render_children)): ?>
+  <?php print $drupal_render_children ?>
+<?php endif; ?>
