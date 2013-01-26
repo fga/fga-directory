3c9cb109d6b31ef9477eddd72c832b7440b1adcf
Thiago de Mello Bueno <thiago@madeofpeople.org>
2013-01-25 23:02:27 -0800

    removed commented out code from template.php

--- sites/all/themes/occupy_directory/template.php
+++ sites/all/themes/occupy_directory/template.php
@@ -14,18 +14,10 @@
 /* alpha does define alpha_exclude_options($theme),
  * but it's more staightforward to exclude our module styles here
  */
+
 function occupy_directory_css_alter(&$css) {
   if (module_exists('twitter_pull')) {
     unset($css[drupal_get_path('module', 'twitter_pull') . '/twitter-pull-listing.css']);
   }
 }
 
-
-
-
-// function occupy_directory_preprocess_comment_wrapper(&$variables) {
-//   dsm($variables);  // pretty print array using Krumo to messages
-//   $variables['title_suffix'] = t('<p>This is a community managed project that relies on people like you to keep the data current!
-//     if something is incomplete or incorrect please suggest an edit using the form below. Edits will typically be reviews and implemented
-//     within 24 hours.</p>');
-// }
