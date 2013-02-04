fba07ba13dd66cfa70017b6afa1977435c654e12
Thiago de Mello Bueno <thiago@madeofpeople.org>
2013-02-04 15:25:41 -0800

    setting up content wrapper for final node implementation

--- sites/all/themes/occupy_directory/templates/comment-wrapper.tpl.php
+++ sites/all/themes/occupy_directory/templates/comment-wrapper.tpl.php
@@ -38,22 +38,25 @@
 ?>
 <div id="comments" class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <?php if ($content['comments'] && $node->type != 'forum'): ?>
-    <?php print render($title_prefix); ?>
-    <h2 class="title"><?php print t('Suggested Edit(s)'); ?></h2>
-    <?php print render($title_suffix); ?>
 
-    <div class="comment-instructions">
+<!--     <div class="comment-instructions">
       <p>This is a community managed project that relies on people like you to keep the data current!
         If something is incomplete or incorrect please suggest an edit using the form below.
         Edits will typically be reviewed and implemented within 24 hours.</p>
    </div>
-  <?php endif; ?>
+ -->
+   <?php endif; ?>
 
-  <?php if ($content['comment_form']): ?>
-    <h2 class="title comment-form"><?php print t('Suggest an Edit'); ?></h2>
-    <?php print render($content['comment_form']); ?>
-  <?php endif; ?>
+    <?php if ($content['comment_form']): ?>
+      <h2 class="title comment-form"><?php print t('Suggest an Edit'); ?></h2>
+      <?php print render($content['comment_form']); ?>
+    <?php endif; ?>
 
-  <?php print render($content['comments']); ?>
+  <div class="comment-log">
+    <?php print render($title_prefix); ?>
+    <h2 class="title"><?php print t('Suggested Edit(s)'); ?></h2>
+    <?php print render($title_suffix); ?>
+   <?php print render($content['comments']); ?>
+  </div>
 
 </div>
