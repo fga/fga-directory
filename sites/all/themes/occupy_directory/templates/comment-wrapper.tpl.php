6c3e16cdf747a5aa9a0f1c892c6b7ad9f37a366d
Thiago de Mello Bueno <thiago@madeofpeople.org>
2013-02-19 13:04:39 -0800

    comments wrapper updates

--- sites/all/themes/occupy_directory/templates/comment-wrapper.tpl.php
+++ sites/all/themes/occupy_directory/templates/comment-wrapper.tpl.php
@@ -37,16 +37,14 @@
  */
 ?>
 <div id="comments" class="<?php print $classes; ?>"<?php print $attributes; ?>>
-  <?php 
-  /*
+  <?php /*
   if ($content['comments'] && $node->type != 'forum'): ?>
   <div class="comment-instructions">
   <p>This is a community managed project that relies on people like you to keep the data current!
   If something is incomplete or incorrect please suggest an edit using the form below.
   Edits will typically be reviewed and implemented within 24 hours.</p>
   </div>
-  <?php endif; 
-    */?>
+  <?php endif;*/?>
   <div class="comment-form-container">
     <?php if ($content['comment_form']): ?>
       <h2 class="title comment-form"><?php print t('Suggest an Edit'); ?></h2>
@@ -54,10 +52,10 @@
     <?php endif; ?>
   </div>
 
-  <a id="suggested-edit-log-toggle" class="icon toggle-comments" href="#"><?php print t( 'Toggle previous suggestions' );?></a>
+  <a title="See previously suggested comments…" id="suggested-edit-log-toggle" class="icon toggle-comments" href="#"><?php print t( 'Toggle previous suggestions' );?></a>
   <div id="suggested-edit-log">
     <?php print render($title_prefix); ?>
-    <h2 class="title"><?php print t('Previously Suggested Edit(s)'); ?></h2>
+    <h3 class="title"><?php print t('Previously Suggested Edit(s)'); ?></h3>
     <?php print render($title_suffix); ?>
    <?php print render($content['comments']); ?>
   </div>
